19adfe89250df2c5a20c302633a466847f154f3c
Yugandhar Yelai <63305302+Tech-nransom@users.noreply.github.com>
2020-12-16 10:23:13 +0530

    added update,insert function
    added Insert in middle and update function

--- linkedList.py
+++ linkedList.py
@@ -22,11 +22,15 @@ class linkedList:
 		print(str_ll)
 
 	def insert_end(self,data):
-		itr = self.head
-		while itr.next:
-			itr = itr.next
-		itr.next = Node(data)
+		try:
+			itr = self.head
+			while itr.next:
+				itr = itr.next
+			itr.next = Node(data)
+		except Exception as exc:
+			print("linkedList not started")
 
+      
 	def get_length(self):
 
 		if self.head is None:
@@ -38,22 +42,51 @@ class linkedList:
 			cnt += 1
 			itr = itr.next
 		return cnt
+	def update_at(self,data,position):
+		if 0<position<=self.get_length():
+			
+			itr = self.head
+			cnt= 1
+			while cnt != position:
+				cnt += 1
+				itr = itr.next
+			itr.data = data
+		else:
+			print("update at position %d not possible"%position)
+			return 
 
+	def insert_after(self,data,position):
+		if 1<position<self.get_length():
+			itr = self.head
+			cnt = 1
+			while cnt != position:
+				cnt += 1
+				itr = itr.next
+			node = Node(data,itr.next)
+			itr.next = node
+		else:
+			print("Insert is not in Middle")
+			return
 
 if __name__ == "__main__":
 
 	ll = linkedList()
-	print(ll.get_length())
+	print("The Length of the linkedList currently is "+str(ll.get_length()))
 	ll.display()
 	ll.insert_beg(10)
 	ll.insert_beg(20)
 	ll.insert_beg(30)
 	ll.display()
-	print(ll.get_length())
+	print("The Length of the linkedList currently is "+str(ll.get_length()))
 	ll.insert_end(40)
 	ll.display()
-	print(ll.get_length())
+	print("The Length of the linkedList currently is "+str(ll.get_length()))
 	ll.insert_beg(90)
 	ll.display()
 	ll.insert_end(100)
 	ll.display()
+	ll.update_at(77,2)
+	ll.display()
+	ll.insert_after(55,5)
+	ll.display()
+	print("The Length of the linkedList currently is "+str(ll.get_length()))
